fd932698d0fc45725f32591cb6cd14403dc6a369
oiweiwei <oiweiweiei@gmail.com>
2026-09-15 17:48:54 +0200

    codegen: positive signed integer as success code

--- examples/demo/myidl/idcominterface/v0/v0.go
+++ examples/demo/myidl/idcominterface/v0/v0.go
@@ -84,7 +84,7 @@ func (o *xxx_DefaultIDcomInterfaceClient) TestDcomCallWithBase(ctx context.Conte
 	}
 	out := &TestDcomCallWithBaseResponse{}
 	out.xxx_FromOp(ctx, op)
-	if op.Return != int32(0) {
+	if op.Return < int32(0) {
 		return out, fmt.Errorf("%s: %w", op.OpName(), o.cc.Error(ctx, op.Return))
 	}
 	return out, nil
